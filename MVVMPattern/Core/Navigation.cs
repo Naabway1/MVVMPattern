336e1921d623128633eacd5f71ebf48838290202
Naabway <naabway2@gmail.com>
2025-02-07 19:46:03 +0500

    Добавьте файлы проекта.

---
+++ MVVMPattern/Core/Navigation.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Windows.Controls;
+using System;
+using MVVMPattern.View;
+
+public class Navigation
+{
+    private readonly Frame _frame;
+    public string Title { get; set; }
+
+    public Navigation(Frame frame)
+    {
+        _frame = frame;
+    }
+
+    public void NavigateTo(object param, object dataContext)
+    {
+        string pageName = param.ToString();
+        Page page;
+
+        switch (pageName)
+        {
+            case "AuthPage":
+                page = new AuthPage();
+                break;
+            case "MainPage":
+                page = new MainPage();
+                break;
+            default:
+                throw new ArgumentException("Неизвестная страница ", nameof(pageName));
+        }
+
+        page.DataContext = dataContext;
+        _frame.Navigate(page);
+    }
+}
---
+++ MVVMPattern/Model/Task.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMPattern.Model
+{
+    internal class Task
+    {
+        private int _id;
+        private string _name;
+        private string _descriptionOfProblem;
+
+        public Task(int id, string name, string descriptionOfProblem)
+        {
+            _id = id;
+            _name = name;
+            _descriptionOfProblem = descriptionOfProblem;
+        }
+    }
+}
